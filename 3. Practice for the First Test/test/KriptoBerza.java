f045f77b3355e478d57abb92f26a31892c0d45a3
Rastko Dimitrijevic <rastkodev@gmail.com>
2023-12-29 12:53:29 +0100

    Update KriptoBerza.java

--- 3. Practice for the First Test/test/KriptoBerza.java
+++ 3. Practice for the First Test/test/KriptoBerza.java
@@ -1,5 +1,17 @@
 package test1;
 
+/*Napisati klasu KriptoBerza koja nasleđuje apstraktnu klasu Berza i implementira 
+	interfejs RadSaTransakcijama. Klasa ima atribute listaTrgovaca (tipa ArrayList<Trgovac>), 
+	listaKriptoValuta (tipa ArrayList<KriptoValuta>) i listaTransakcija (tipa ArrayList<Transakcija>), 
+	konstruktor (napomena: roditeljska klasa ima atribute: naziv, tipa String; grad, tipa String; 
+	nazivIndeksa tipa String; vrednostIndeksa, tipa double) i metode za pribavljanje i postavljanje 
+	atributa (getere i setere). Interfejs RadSaTransakcijama ima samo metodu izvrsiTransakciju koja 
+	vlasnistvo nad kripto valutom iz niza listaKriptoValuta prenosi sa jednog na drugog trgovca 
+	iz niza listaTrgovaca. Prepostaviti da je svaki element niza listaKriptoValuta kripto valuta 
+	koji kao svoje atribute sadrži vlasnika, količinu i naziv valute. Kod svih atributa i metoda 
+	navesti i odgovarajuće pristupne atribute.
+*/
+
 import java.util.ArrayList;
 
 public class KriptoBerza extends Berza implements RadSaTransakcijama {
