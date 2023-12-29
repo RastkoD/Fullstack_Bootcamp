0482ace0855d08e878dec4e50a0f3725ae32c990
Rastko Dimitrijevic <rastkodev@gmail.com>
2023-12-29 12:54:02 +0100

    Update CryptoReader.java

--- 3. Practice for the First Test/test/CryptoReader.java
+++ 3. Practice for the First Test/test/CryptoReader.java
@@ -1,5 +1,11 @@
 package test1;
 
+/*Tekstualna datoteka sadrži podatke o kripto valutama kojima se trguje na kriptoberzi u sledećem 
+	obliku: 5f99h1ew9fwq 5.00 Litecoin. Prvi parametar je jedinstveni identifikator korisnika, drugi je 
+	količina kripto valute, a treći naziv kripto valute. Napisati funkciju kojom se iz ove datoteke mogu 
+	učitati podaci o proizvoljnom broju kripto valuta u dinamički niz listaKriptoValuta.
+*/	
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
